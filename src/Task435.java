2af872b72bf39e9363137ceae1b65791fbe48493
7alias <xc.navi@gmail.com>
2021-12-06 10:34:57 +0200

    4.3.1 4.3.2 4.3.3 4.3.4 4.3.5

--- src/Task435.java
+++ src/Task435.java
@@ -4,26 +4,16 @@ import java.util.regex.Pattern;
 public class Task435 {
 
     protected static void task() {
-
         //Задание4.3.5
-
 //                Напишите регулярное выражение,которое находит все английские слова в тексте,начинающиеся и заканчивающиеся на букву s.Например,в тексте:
-
         String text = "Regular Expressions or Regex is an API for defining String patterns that can be used for searching,manipulating and editing a text.It is widely used to define a constraint on strings such as a password.Regular Expressions are provided under java.util.regex package.";
-
-        Pattern pattern = Pattern.compile("\\bs.\\w+s");
-
-
+        Pattern pattern = Pattern.compile("\\bs\\w+s");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             int start = matcher.start();
             int end = matcher.end();
             System.out.println(text.substring(start, end));
         }
-
-
         //            должно быть найдено только слово strings.
-
-
     }
 }
